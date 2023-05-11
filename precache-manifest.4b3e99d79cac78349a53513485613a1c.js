03e2261cd7e237e32059f03ec1849d5874cd028e
Austin Wright <austindavidw@yahoo.com>
2023-05-11 09:51:47 -0500

    Updates

---
+++ precache-manifest.4b3e99d79cac78349a53513485613a1c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f8f590e72c4efcd33111c6b3996184",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "b1b29cf9efb7207dbe7d",
+    "url": "/note-app/static/js/2.df0d89d3.chunk.js"
+  },
+  {
+    "revision": "f8c124d7f89ae69def24",
+    "url": "/note-app/static/js/main.c96cf323.chunk.js"
+  },
+  {
+    "revision": "8a1189786d4e8ccb3523",
+    "url": "/note-app/static/js/runtime-main.8d2f110d.js"
+  }
+]);
